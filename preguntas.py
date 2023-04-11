4e8a551db6acaaae3616dd6b2fd088f7dcc343d3
Vassa94 <vassallo1994@gmail.com>
2023-04-11 12:00:16 -0300

    eliminado archivo sin uso

--- preguntas.py
+++
@@ -1,5 +0,0 @@
-preguntas_y_respuestas = {
-    'capital de España': 'La capital de España es Madrid.',
-    'presidente de Estados Unidos': 'El presidente de Estados Unidos es Joe Biden.',
-    # Agrega aquí más preguntas y respuestas
-}
