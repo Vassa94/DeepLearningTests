e8bd1b246e3a240c50e90c173a56d8ce44c2ef55
Vassa94 <vassallo1994@gmail.com>
2023-04-11 11:03:49 -0300

    inicializando pruebas

--- programa.py
+++ programa.py
@@ -1,11 +1,30 @@
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
-nltk.download('all')
+# Descarga los recursos de NLTK necesarios
+nltk.download('punkt')
+nltk.download('stopwords')
 
-from nltk.tokenize import word_tokenize
+# Función para responder preguntas
+def responder_pregunta(pregunta):
+    # Tokeniza la pregunta en palabras y oraciones
+    palabras = word_tokenize(pregunta)
+    oraciones = sent_tokenize(pregunta)
 
-sentence = "Hola, ¿cómo estás?"
+    # Elimina las palabras vacías y las stopwords
+    palabras = [palabra for palabra in palabras if palabra.lower() not in stopwords.words('spanish')]
 
-tokens = word_tokenize(sentence)
+    print('Palabras después de eliminar las stopwords:', palabras)
+    
+    # Agrega reglas para responder preguntas específicas
+    if 'capital' in palabras and 'españa' in palabras:
+        return 'La capital de España es Madrid.'
+    else:
+        return 'Lo siento, no sé la respuesta a esa pregunta.'
 
-print(tokens)
+# Ejemplo de uso de la función
+pregunta = '¿Cuál es la capital de España?'
+respuesta = responder_pregunta(pregunta)
+print(respuesta)
+
